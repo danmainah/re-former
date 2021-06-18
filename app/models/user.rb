7c1de92cec0ee89dd3fc84dc004c121163f405cf
unknown <danielmainawaweru@gmail.com>
2021-06-18 15:31:13 +0300

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, presence: true
+    validates :email, presence: true
+    validates :passoword, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :users , only: [:new ,:create]
+  resources :articles
+  get "/articles/:id", to: "articles#show"
+  get 'articles', to: 'articles#index'
   get "/users", to: "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
