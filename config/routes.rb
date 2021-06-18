cd30ee8a41612766a9ea3f9ea2dca9f32149a66d
unknown <danielmainawaweru@gmail.com>
2021-06-18 17:17:12 +0300

    Change form to use tags and form with

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :users , only: [:new ,:create]
+  resources :users, only: [:new, :create]
   resources :articles
-  get "/articles/:id", to: "articles#show"
-  get 'articles', to: 'articles#index'
-  get "/users", to: "users#index"
-  get "/new", to: "users#new"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
