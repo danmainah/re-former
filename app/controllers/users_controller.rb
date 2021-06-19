fa4851ec9e39d7edd675fab719ddb8a487acc907
unknown <danielmainawaweru@gmail.com>
2021-06-19 10:31:18 +0300

    Fix editing and deleting errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path
+      redirect_to @user
     else
       render :new
     end
@@ -27,16 +27,23 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-    if @user.update(article_params)
+    if @user.update(user_params)
       redirect_to @user
     else
       render :edit
     end
+    # @user = User.find(params[:id])
+
+    # if @user.update(user_params)
+    #   redirect_to @user
+    # else
+    #   render :edit
+    # end
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
+    @user = User.find(params[:id])
+    @user.destroy
 
     redirect_to root_path
   end
