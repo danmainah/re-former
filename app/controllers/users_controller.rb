6ec26c1dc16f74a1c7fdc01647b63806487378ca
unknown <danielmainawaweru@gmail.com>
2021-06-19 18:29:53 +0300

    Add redirect to edit user path as requested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to(@user)
+      redirect_to edit_user_path(@user)
     else
       render :edit
     end
